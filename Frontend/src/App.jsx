f8a3be357caaf87c1b0feac87df542fadea04473
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2025-01-21 18:10:44 -0300

    Add initial `<Route>` configuration for frontend project in `App.jsx`

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,22 +1,28 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { LandingPage, UserLoginPage, UserRegisterPage } from "./pages"
+
 function App() {
+    const WIP_MESSAGE = "Página aún en construcción..."
+    const ERROR_MESSAGE = "¡UPS! Esa página no existe..."
+
     return (
-        <div className="grid h-screen w-full grid-rows-[1fr_8fr_1fr] items-center bg-black text-white">
-            <header className="flex items-center justify-center border-b-2 border-b-white py-4">
-                <h1>AVANTI</h1>
-            </header>
-            <main className="grid place-items-center">
-                <div className="rounded-6xl m-4 flex flex-col gap-4 border-2 border-solid border-white p-8">
-                    <h2 className="">Se vienen cositas...</h2>
-                    <p>Un paso a la vez, todo se puede</p>
-                    <button className="min-h-12 min-w-12 rounded-2xl bg-white text-black hover:bg-lime-600 hover:text-white active:bg-gray-700 active:text-white">
-                        ¡Entendido!
-                    </button>
-                </div>
-            </main>
-            <footer className="flex items-center justify-center border-t-2 border-t-white py-4">
-                <h2>NoCountry | Equipo-s20-17-webapp</h2>
-            </footer>
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<LandingPage />} />
+                <Route path="/search" element={<h2>{WIP_MESSAGE}</h2>} />
+                <Route path="/proyect/:proyectId" element={<h2>{WIP_MESSAGE}</h2>} />
+                <Route path="/checkout/:proyectId" element={<h2>{WIP_MESSAGE}</h2>} />
+                <Route path="/login" element={<UserLoginPage />} />
+                <Route path="/register" element={<UserRegisterPage />} />
+                <Route path="/about" element={<h2>{WIP_MESSAGE}</h2>} />
+                <Route path="/recovery" element={<h2>{WIP_MESSAGE}</h2>} />
+                <Route path="/mission" element={<h2>{WIP_MESSAGE}</h2>} />
+                <Route path="/contact" element={<h2>{WIP_MESSAGE}</h2>} />
+                <Route path="/tips" element={<h2>{WIP_MESSAGE}</h2>} />
+                <Route path="/process" element={<h2>{WIP_MESSAGE}</h2>} />
+                <Route path="*" element={<h2>{ERROR_MESSAGE}</h2>} />
+            </Routes>
+        </BrowserRouter>
     )
 }
 
