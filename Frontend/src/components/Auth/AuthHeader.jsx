f80806bf3087c962bdb033d1d435d9c02235f138
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2025-01-21 17:26:28 -0300

    Add primitive AuthHeader.jsx implementation

---
+++ Frontend/src/components/Auth/AuthHeader.jsx
@@ -0,0 +1,7 @@
+export function AuthHeader() {
+    return (
+        <header className="flex h-full items-center justify-center border-b-2 border-b-white bg-[#121212d0] py-4">
+            <h1>AVANTI</h1>
+        </header>
+    )
+}
