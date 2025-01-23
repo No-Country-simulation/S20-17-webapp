d0fb20b28f764ff1db956c07dd5500d3728b2086
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2025-01-22 23:46:57 -0300

    Add linear-gradient animation for submit button in LoginForm.jsx

--- Frontend/src/components/Auth/LoginForm.jsx
+++ Frontend/src/components/Auth/LoginForm.jsx
@@ -2,10 +2,7 @@ export function LoginForm() {
     const loginInProcess = false
 
     return (
-        <form
-            className="flex w-full max-w-md flex-col items-center"
-            id="loginForm"
-        >
+        <form className="flex w-full max-w-md flex-col items-center" id="loginForm">
             <input
                 id="loginFormEmail"
                 type="email"
@@ -22,7 +19,7 @@ export function LoginForm() {
             />
             <button
                 type="submit"
-                className="w-full rounded-2xl bg-[#1f497d] p-3 font-medium text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+                className="w-full rounded-2xl bg-[linear-gradient(92deg,_#1f497d,_#2e806e,_#005839)] bg-[length:200%_200%] bg-left p-3 font-medium text-white shadow-md transition-[background-position] duration-500 ease-in-out hover:bg-right focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
             >
                 {loginInProcess ? "AGUARDA UNOS INSTANTES" : "Iniciar Sesión"}
             </button>
