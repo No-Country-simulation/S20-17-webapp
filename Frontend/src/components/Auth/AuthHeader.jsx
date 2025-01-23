2a5c557bd31e1986a0f152a7421dea1617527d8c
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2025-01-23 01:13:07 -0300

    Fix minor SEO issue with html tags in AuthHeader.jsx

--- Frontend/src/components/Auth/AuthHeader.jsx
+++ Frontend/src/components/Auth/AuthHeader.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom"
 export function AuthHeader() {
     return (
         <header className="flex h-full items-center justify-center border-b-2 border-b-white bg-[#121212d0] py-4">
-            <h1>
+            <h2>
                 <Link to="/">AVANTI</Link>
-            </h1>
+            </h2>
         </header>
     )
 }
