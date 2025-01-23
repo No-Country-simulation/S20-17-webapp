842eac0684e138eb913aaf214e998ebb7dfd3de7
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2025-01-22 23:46:27 -0300

    Add linear-gradient animations for Tailwind

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -7,6 +7,22 @@ export default {
                 "4xl": "2rem",
                 "6xl": "3rem",
             },
+            backgroundPosition: {
+                left: "0% 50%",
+                right: "100% 50%",
+            },
+            animation: {
+                "gradient-flow": "gradient-flow 1.5s ease forwards",
+            },
+            keyframes: {
+                "gradient-flow": {
+                    "0%": { backgroundPosition: "0% 50%" },
+                    "100%": { backgroundPosition: "100% 50%" },
+                },
+            },
+            backgroundSize: {
+                "200%": "200%",
+            },
         },
     },
     plugins: [],
