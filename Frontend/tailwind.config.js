6d2c600d38a47cdd372937821897f56c584f9e4f
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2025-01-23 00:34:10 -0300

    Add gradient-avanti in Tailwind config

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -7,6 +7,12 @@ export default {
                 "4xl": "2rem",
                 "6xl": "3rem",
             },
+            backgroundImage: {
+                "gradient-avanti": "linear-gradient(92deg,#1f497d,#2e806e,#005839)",
+            },
+            backgroundClip: {
+                text: "text",
+            },
             backgroundPosition: {
                 left: "0% 50%",
                 right: "100% 50%",
