f08f75b252619ae07d9931e6c5869ae9cca3ae6c
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2025-01-25 21:02:23 -0300

    Fix password regex in regex.js

--- Frontend/src/utils/regex.js
+++ Frontend/src/utils/regex.js
@@ -1,15 +1,20 @@
 export const regexFormPatterns = {
-    password: /^(?=.\d)(?=.[a-z])(?=.[A-Z])(?=.[@#$%^&+=!]).{8,}$/,
+    password: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!]).{8,}$/gm,
+    // password: /^(?=.\d)(?=.[a-z])(?=.[A-Z])(?=.[@#$%^&+=!]).{8,}$/,
 
     /*
         Desglose de la Expresión Regular:
             ^: Comienzo de la línea.
-            (?=.\d): Al menos un dígito.
-            (?=.[a-z]): Al menos una letra minúscula.
-            (?=.[A-Z]): Al menos una letra mayúscula.
-            (?=.[@#$%^&+=!]): Al menos un carácter especial (de los listados).
-            .{8,}: Al menos 8 caracteres en total.
+            (?=.*\d): Asegura que haya al menos un dígito (0-9).
+            (?=.*[a-z]): Asegura que haya al menos una letra minúscula (a-z).
+            (?=.*[A-Z]): Asegura que haya al menos una letra mayúscula (A-Z).
+            (?=.*[@#$%^&+=!]): Asegura que haya al menos un carácter especial 
+                (de los siguientes: @, #, $, %, ^, &, +, =, !).
+            .{8,}: Asegura que la contraseña tenga al menos 8 caracteres en total.
             $: Fin de la línea.
+        Opciones:
+            g: Búsqueda global (puede coincidir en múltiples líneas si es necesario).
+            m: Modo multilínea (permite que ^ y $ coincidan al inicio y final de cada línea).
     */
 
     email: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
