61d3b0eae25233d8fc2ccb1542e415fc24841453
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2025-01-26 03:28:37 -0300

    Add primitive functionality for RegisterForm.jsx

--- Frontend/src/components/Auth/RegisterForm.jsx
+++ Frontend/src/components/Auth/RegisterForm.jsx
@@ -1,46 +1,75 @@
+import { useState } from "react"
+import { useForm } from "react-hook-form"
+import { regexFormPatterns } from "../../utils/regex.js"
+
 export function RegisterForm() {
-    const registerInProcess = false
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        watch,
+    } = useForm()
+    const [registerInProcess, setRegisterInProcess] = useState(false)
 
     return (
-        <form className="flex w-full max-w-md flex-col items-center" id="registerForm">
+        <form
+            className="flex w-full max-w-md flex-col items-center"
+            id="registerForm"
+            onSubmit={handleSubmit((data) => console.log(JSON.stringify(data)))}
+        >
             <input
                 id="registerFormName"
                 type="text"
                 placeholder="Nombre"
-                required
-                className="border-avanti-black focus:ring-avanti-light-green focus:border-avanti-light-green mb-3 w-full rounded-2xl border p-3 focus:outline-none focus:ring-2"
+                autoComplete="true"
+                {...register("name", { required: true, maxLength: 255 })}
+                className="mb-3 w-full rounded-2xl border border-avanti-black p-3 focus:border-avanti-light-green focus:outline-none focus:ring-2 focus:ring-avanti-light-green"
             />
             <input
                 id="registerFormEmail"
                 type="email"
                 placeholder="Correo electrónico"
-                required
-                className="border-avanti-black focus:border-avanti-light-green focus:ring-avanti-light-green mb-3 w-full rounded-2xl border p-3 focus:outline-none focus:ring-2"
+                autoComplete="true"
+                {...register("email", { required: true, pattern: regexFormPatterns.email })}
+                className="mb-3 w-full rounded-2xl border border-avanti-black p-3 focus:border-avanti-light-green focus:outline-none focus:ring-2 focus:ring-avanti-light-green"
             />
+            {/* {errors.email && errors.email.type === "pattern" && <span role="alert">FORMATO INVALIDO</span>} */}
             <input
+                {...register("emailConfirmation")}
                 id="registerFormEmailConfirmation"
                 type="email"
                 placeholder="Confirmar correo electrónico"
-                required
-                className="border-avanti-black focus:border-avanti-light-green focus:ring-avanti-light-green mb-3 w-full rounded-2xl border p-3 focus:outline-none focus:ring-2"
+                {...register("emailConfirmation", { required: true, validate: (value) => watch("email") === value })}
+                className="mb-3 w-full rounded-2xl border border-avanti-black p-3 focus:border-avanti-light-green focus:outline-none focus:ring-2 focus:ring-avanti-light-green"
             />
+            {/* {errors.emailConfirmation && errors.emailConfirmation.type === "validate" && (
+                <span role="alert">NO COINCIDEN</span>
+            )} */}
             <input
+                {...register("password")}
                 id="registerFormPassword"
                 type="password"
                 placeholder="Contraseña"
-                required
-                className="border-avanti-black focus:border-avanti-light-green focus:ring-avanti-light-green mb-3 w-full rounded-2xl border p-3 focus:outline-none focus:ring-2"
+                {...register("password", { required: true, pattern: regexFormPatterns.password })}
+                className="mb-3 w-full rounded-2xl border border-avanti-black p-3 focus:border-avanti-light-green focus:outline-none focus:ring-2 focus:ring-avanti-light-green"
             />
+            {/* {errors.password && errors.password.type === "pattern" && <span role="alert">FORMATO INVALIDO</span>} */}
             <input
                 id="registerFormPasswordConfirmation"
                 type="password"
                 placeholder="Confirmar contraseña"
-                required
-                className="border-avanti-black focus:border-avanti-light-green focus:ring-avanti-light-green mb-3 w-full rounded-2xl border p-3 focus:outline-none focus:ring-2"
+                {...register("passwordConfirmation", {
+                    required: true,
+                    validate: (value) => watch("password") === value,
+                })}
+                className="mb-3 w-full rounded-2xl border border-avanti-black p-3 focus:border-avanti-light-green focus:outline-none focus:ring-2 focus:ring-avanti-light-green"
             />
+            {/* {errors.passwordConfirmation && errors.passwordConfirmation.type === "validate" && (
+                <span role="alert">NO COINCIDEN</span>
+            )} */}
             <button
                 type="submit"
-                className="focus:border-avanti-light-green focus:ring-avanti-light-green bg-gradient-avanti w-full rounded-2xl bg-[length:200%_200%] bg-left p-3 font-medium text-white shadow-md transition-[background-position] duration-500 ease-in-out hover:bg-right focus:border-none focus:outline-none focus:ring-2"
+                className="w-full rounded-2xl bg-gradient-avanti bg-[length:200%_200%] bg-left p-3 font-medium text-white shadow-md transition-[background-position] duration-500 ease-in-out hover:bg-right focus:border-none focus:border-avanti-light-green focus:outline-none focus:ring-2 focus:ring-avanti-light-green"
             >
                 {registerInProcess ? "AGUARDA UNOS INSTANTES" : "Registrarse en Avanti"}
             </button>
