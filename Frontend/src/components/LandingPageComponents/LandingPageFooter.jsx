c0ba367572816cedccb66354401b510a8266420c
Federico Correa <elgatoberdaguer@gmail.com>
2025-02-04 16:16:51 -0300

    footer detalles y mejoras

--- Frontend/src/components/LandingPageComponents/LandingPageFooter.jsx
+++ Frontend/src/components/LandingPageComponents/LandingPageFooter.jsx
@@ -1,13 +1,50 @@
 import { Link } from "react-router-dom"
+import React, { useState, useEffect } from "react"
+
+const TestimonialSlider = () => {
+    // Testimonios a mostrar
+    const testimonios = [
+        { texto: '"Excelente servicio, totalmente recomendable!"', autor: "Juan Pérez" },
+        { texto: '"Un equipo increíble que te ayuda en cada paso."', autor: "Ana Gómez" },
+        { texto: '"Una experiencia única, ¡estoy muy contento con el resultado!"', autor: "Carlos López" },
+        { texto: '"Recomiendo totalmente esta empresa. ¡Impresionante trabajo!"', autor: "Laura Sánchez" },
+    ]
+
+    // Estado para manejar el índice del testimonio actual
+    const [index, setIndex] = useState(0)
+
+    // Cambiar el testimonio cada 4 segundos
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setIndex((prevIndex) => (prevIndex + 1) % testimonios.length)
+        }, 4000) // Cambia cada 4 segundos
+
+        return () => clearInterval(interval) // Limpiar el intervalo al desmontar el componente
+    }, [])
+
+    return (
+        <div className="flex flex-col items-center gap-4">
+            <p className="text-lg font-medium text-avanti-white">Lo que dicen nuestros usuarios</p>
+            <div className="flex flex-col items-center gap-2">
+                <p className="italic text-avanti-white">{testimonios[index].texto}</p>
+                <p className="font-semibold text-avanti-white">- {testimonios[index].autor}</p>
+            </div>
+        </div>
+    )
+}
 
 export function LandingPageFooter() {
     return (
         <>
-            <footer className="body-font flex h-20 w-full items-center justify-between gap-4 bg-avanti-blue p-4 text-white">
-                <div className="h-full">
-                    <img src="src/assets/avanti-logo.svg" alt="Logo de Avanti" className="h-full" />
-                </div>
-                <p className="font-semibold text-avanti-white">© 2025 Avanti</p>
+            {/* <footer className="body-font flex h-32 w-full items-center justify-between gap-4 bg-avanti-blue p-4 text-white" style={{
+                        backgroundImage: "url('./public/bg.png')",
+                        backgroundSize: "cover",
+                        backgroundPosition: "center",
+                    }}>
+                <figure className="h-full">
+                    <img src="src/assets/avanti-logo.svg" alt="Logo de Avanti" className="h-[60px] mt-6" />
+                </figure>
+                <p className="font-semibold text-avanti-white text-xl">© 2025 Avanti</p>
                 <div className="flex items-center gap-4">
                     <Link
                         className="text-avanti-white"
@@ -28,6 +65,82 @@ export function LandingPageFooter() {
                     >
                         <img src="src/assets/instagram-logo.svg" alt="Logo de Instagram" className="h-7 w-7" />
                     </Link>
+                </div>
+            </footer> */}
+            <footer
+                className="body-font flex w-full flex-col items-center bg-avanti-blue p-8 text-white"
+                style={{
+                    backgroundImage: "url('./public/bg.png')",
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                    backgroundBlendMode: "overlay",
+                }}
+            >
+                {/* Sección superior con logo y texto */}
+                <div className="flex w-full items-center justify-between">
+                    <figure className="flex items-center">
+                        <img src="src/assets/avanti-logo.svg" alt="Logo de Avanti" className="h-[60px]" />
+                    </figure>
+                    <p className="text-xl font-semibold text-avanti-white">© 2025 Avanti</p>
+                </div>
+
+                {/* Sección central para organizar mejor el contenido */}
+                <div className="mt-8 flex w-full items-center justify-between">
+                    {/* Enlaces rápidos */}
+                    <div className="flex flex-col items-start gap-4">
+                        <p className="text-lg font-bold text-avanti-white">Enlaces rápidos</p>
+                        <ul className="flex flex-col gap-2">
+                            <li>
+                                <Link className="text-avanti-white hover:text-avanti-light-green">
+                                    Sobre nosotros
+                                </Link>
+                            </li>
+                            <li>
+                                <Link className="text-avanti-white hover:text-avanti-light-green">
+                                    Contacto
+                                </Link>
+                            </li>
+                            <li>
+                                <Link className="text-avanti-white hover:text-avanti-light-green">
+                                    Términos y condiciones
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+
+                    {/* Testimonios dinámicos */}
+                    <TestimonialSlider />
+
+                    {/* Redes sociales */}
+                    <div className="flex flex-col items-center gap-2">
+                        <p className="text-lg font-medium text-avanti-white">Síguenos</p>
+                        <div className="flex gap-4">
+                            <Link
+                                className="text-avanti-white"
+                                to="https://www.facebook.com"
+                                target="blank"
+                                rel="noopener noreferrer"
+                            >
+                                <img src="src/assets/facebook-logo.svg" alt="Logo de Facebook" className="h-7 w-7 transition-all duration-300 ease-in-out hover:blur-[2px]" />
+                            </Link>
+                            <Link
+                                className="text-avanti-white"
+                                to="https://x.com/"
+                                target="blank"
+                                rel="noopener noreferrer"
+                            >
+                                <img src="src/assets/x-logo.svg" alt="Logo de X / Twitter" className="h-7 w-7 transition-all duration-300 ease-in-out hover:blur-[2px]" />
+                            </Link>
+                            <Link
+                                className="text-avanti-white"
+                                to="https://instagram.com/"
+                                target="blank"
+                                rel="noopener noreferrer"
+                            >
+                                <img src="src/assets/instagram-logo.svg" alt="Logo de Instagram" className="h-7 w-7 transition-all duration-300 ease-in-out hover:blur-[2px]" />
+                            </Link>
+                        </div>
+                    </div>
                 </div>
             </footer>
         </>
--- Frontend/src/components/SearchPageComponents/SearchPageContent.jsx
+++ Frontend/src/components/SearchPageComponents/SearchPageContent.jsx
@@ -1,21 +1,3 @@
-
-
-// export function SearchPageContent() {
-//     return (
-//         <>
-//             <div className="relative mx-auto flex w-2/4 flex-col items-center justify-center">
-//                 <h1 className="mb-8 py-5 text-center text-4xl font-semibold text-[#1F497D]">Buscar Proyectos</h1>
-//                 <input
-//                     type="text"
-//                     id="hero-field"
-//                     name="hero-field"
-//                     className="mx-auto w-1/2 rounded border border-gray-300 bg-gray-100 bg-opacity-50 px-3 py-1 text-base leading-8 text-gray-700 outline-none transition-colors duration-200 ease-in-out focus:border-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200"
-//                 />
-//             </div>
-//         </>
-//     )
-// }
-
 import React, { useState } from 'react';
 
 const cards = [
@@ -122,7 +104,7 @@ export function SearchPageContent() {
                     type="text"
                     id="hero-field"
                     name="hero-field"
-                    className="mx-auto w-1/2 rounded border border-gray-300 bg-gray-100 bg-opacity-50 px-3 py-1 text-base leading-8 text-gray-700 outline-none transition-colors duration-200 ease-in-out focus:border-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200"
+                    className="mx-auto w-1/2 px-3 py-2 border-avanti-black focus:border-avanti-light-green focus:ring-avanti-light-green mb-3 rounded-2xl border p-3 focus:outline-none focus:ring-2"
                     value={searchQuery}
                     onChange={(e) => setSearchQuery(e.target.value)}
                     placeholder="Buscar por título, descripción o categoría"
