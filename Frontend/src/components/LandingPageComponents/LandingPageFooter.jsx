3ed30e495ca4ac0e760d870f56b9e1cc11629457
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2025-02-04 13:38:51 -0300

    Minor accesibility improvements for LandingPage

--- Frontend/src/components/LandingPageComponents/LandingPageFooter.jsx
+++ Frontend/src/components/LandingPageComponents/LandingPageFooter.jsx
@@ -5,7 +5,11 @@ export function LandingPageFooter() {
         <>
             <footer className="body-font flex h-20 w-full items-center justify-between gap-4 bg-avanti-blue p-4 text-white">
                 <div className="h-full">
-                    <img src="./avanti-logo-footer.svg" alt="Logo de Avanti" className="h-full" />
+                    <img
+                        src="./avanti-logo-footer.svg"
+                        alt="Logo de Avanti - volver a la página de inicio"
+                        className="h-full"
+                    />
                 </div>
                 <p className="font-semibold text-avanti-white">© 2025 Avanti</p>
                 <div className="flex items-center gap-4">
